5a91d36047cf36966f9c2d6de46321c72b36a874
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-04-09 16:42:14 +0600

    private rout done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 
 
@@ -15,7 +16,11 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/blogs' element={
+          <RequireAuth>
+            <Blogs></Blogs>
+          </RequireAuth>
+        }></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { auth } from '../../firebase/firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
+    let navigate = useNavigate();
+    let location = useLocation();
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    let from = location.state?.from?.pathname || "/";
+
+    const handleSigninWithGoogle = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true });
+            })
+    }
     return (
         <div>
             <h3>Please Login</h3>
-            <button onClick={() => signInWithGoogle()}>Login with google</button>
+            <button onClick={handleSigninWithGoogle}>Login with google</button>
             <form>
                 <input type="email" placeholder='enter email' /> <br />
                 <input type="password" placeholder='enter password' /> <br />
